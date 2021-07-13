8ad9b6eb9cfb7c7c8a7da85a696d4d967f0a3d01
Aruna271 <aruaws4practise@gmail.com>
2021-07-13 10:40:08 +0530

    Added a func

--- script.sh
+++ script.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-#Newly added lines
-#added if statement
+#This will give all nums from 1 to 100 by windows user...!!
+#This will give all nums from 1 to 100 by Linux user
+for i in {1..100}; do
+    if [ $(expr $i % 2) -eq 0 ]; then
+    echo "$i is a EVEN Number"
+    else
+    echo "$i is a ODD Number except if it is Zero"
+    fi
+    done 
+
+
+    func(){
+        echo "This is a test function..!!"
+    }
